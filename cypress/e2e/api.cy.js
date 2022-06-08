69ac11b72ab5cc30cae359d58e4d8aae20e07e93
DPelyp <pelup19992@gmail.com>
2022-06-08 14:53:54 +0300

    added tests

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,5 +1,5 @@
-describe("User registartaion Full", () => { 
-  it("Form 1 Registration", () => {
+describe("PET Store Swagger", () => { 
+  it("Add new pet to the store", () => {
     cy.request('POST', 'https://petstore.swagger.io/v2/pet', { 
       id : 12 , name : 'PES', status : 'available' 
     }).then(
@@ -7,9 +7,36 @@ describe("User registartaion Full", () => {
         expect(response.status).to.eq(200),
         expect(response.body).to.have.property('id', 12),
         expect(response.body).to.have.property('name', 'PES'),
-        expect(response.body).to.have.property('status', 'available'),
-        expect(response.body).length(42)
-      }
-    )
+        expect(response.body).to.have.property('status', 'available')
+        // expect(response.body).length(42)
+      })
     })
-  })
+    it("Update an existing pet", () => {
+      cy.request('PUT', 'https://petstore.swagger.io/v2/pet', { 
+        id : 45 , name : 'Lucky', status : 'unavailable' 
+      }).then(
+        (response) => {
+          expect(response.status).to.eq(200),
+          expect(response.body).to.have.property('id', 45),
+          expect(response.body).to.have.property('name', 'Lucky'),
+          expect(response.body).to.have.property('status', 'unavailable')
+          // expect(response.body).length(42)
+        })
+      })
+      it("Finds Pets by status", () => {
+      cy.request('GET', 'https://petstore.swagger.io/v2/pet/findByStatus?status=available', {
+        id : 45
+      }).then(
+        (response) => {
+          expect(response.status).to.eq(200)
+          expect(response.body).to.have.property(length)
+        })
+      })
+      it("Delete Pets by id", () => {
+        cy.request('DELETE', 'https://petstore.swagger.io/v2/pet/45').then(
+          (response) => {
+            expect(response.status).to.eq(200)
+            // expect(response.body).length(493)
+          })
+        })
+    })
